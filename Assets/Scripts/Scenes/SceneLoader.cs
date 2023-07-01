c109f5c6c8a5dd4bc2723b89a8d8cb780c4a9eae
SnappyNVM <112257868+SnappyNVM@users.noreply.github.com>
2023-07-01 20:38:10 +0500

    +Scene managment, fixed and optimized

--- Assets/Scripts/Scenes/SceneLoader.cs
+++ Assets/Scripts/Scenes/SceneLoader.cs
@@ -1,8 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+enum Scenes
+{ 
+    Menu,
+    LevelMenu,
+    DefaultLevel,
+    Level1
+}
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private Scenes _sceneToLoad;
 
+    public void LoadTheScene()
+    {
+        SceneManager.LoadScene(_sceneToLoad.ToString());
+        Time.timeScale = 1.0f;
+    }
+
+    public void LoadItself()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1.0f;
+    }
 }
